835810963dbdd3fe64df70ef99445de97cca608d
Dnyaneshwar Mogal <maulimogal@gmail.com>
2021-09-20 10:17:38 +0530

    sum of digits

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const btn = document.querySelector("#btn");
 
-btn.addEventListener("click", function getValues() {
-  console.log(dateOfBirth.value, luckyNumber.value);
-});
+function checkLuckyBirthday() {
+  const birthDate = dateOfBirth.value;
+  const sum = calculateSum(birthDate);
+  console.log(sum);
+}
+
+function calculateSum(birthDate) {
+  birthDate = birthDate.replaceAll("-", "");
+  let sum = 0;
+  for (let index = 0; index < birthDate.length; index++) {
+    sum = sum + Number(birthDate.charAt(index));
+  }
+  return sum;
+}
+
+btn.addEventListener("click", checkLuckyBirthday);
