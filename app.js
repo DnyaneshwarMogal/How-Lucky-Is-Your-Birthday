e32f62f7fbccc6afebe9c7ccfdf20c8541c1ea19
Dnyaneshwar Mogal <maulimogal@gmail.com>
2021-09-20 09:47:55 +0530

    taking input from user

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
-const btn = document.querySelector("btn");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function getValues() {
+  console.log(dateOfBirth.value, luckyNumber.value);
+});
